f270e7d64cf812a058586fc8de7742325c457d6c
donaldmema <mema.donald@gmail.com>
2022-09-02 10:29:25 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
